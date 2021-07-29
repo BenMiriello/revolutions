370595f832ba4ccf61f5bc393ca70f3800f0bb12
BenMiriello <benmiriello@gmail.com>
2021-07-28 23:45:42 -0400

    WIP: New header

--- resources/scripts/header.jsx
+++ resources/scripts/header.jsx
@@ -2,11 +2,11 @@ const createElement = require('./createElement');
 
 const header = (meta) => (
   <header>
-    <div id='header-cover-container'>
+    <div id='header-title-container'>
       <h1>{meta.title}</h1>
-      <div id='sub-header-container'>
-        <h3>a&nbsp;weekly&nbsp;podcast&nbsp;exploring great&nbsp;political&nbsp;revolutions</h3>
-      </div>
+    </div>
+    <div id='sub-header-container'>
+      {/* <h3>a&nbsp;weekly&nbsp;podcast&nbsp;exploring great&nbsp;political&nbsp;revolutions</h3> */}
     </div>
   </header>
 )
