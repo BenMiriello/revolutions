673d6688e99f636cfea9d23417e2ad61534d8197
BenMiriello <benmiriello@gmail.com>
2021-08-01 20:31:18 -0400

    refactor: shorten placeholderPages

--- resources/scripts/placeholderPages.jsx
+++ resources/scripts/placeholderPages.jsx
@@ -1,29 +1,11 @@
 const createElement = require('./createElement');
 
-const supportPage = ({podcast}) => {
-  return(<div>TEST SUPPORT</div>);
-};
-
-const bibliographyPage = ({podcast}) => {
-  return(<div>TEST BIBLIOGRAPHY</div>);
-};
-
-const aboutMePage = ({podcast}) => {
-  return(<div>TEST ABOUTME</div>);
-};
-
-const theHistoryOfRomePage = ({podcast}) => {
-  // redirect w/ message about redirecting & link
-  return(<div>TEST THoR</div>);
-};
-
-const audibleRecsPage = ({podcast}) => {
-  return(<div>TEST AUDIBLE RECS</div>);
-};
-
-const contactPage = ({podcast}) => {
-  return(<div>TEST CONTACT</div>);
-};
+const supportPage = ({podcast}) => <div>TEST SUPPORT</div>;
+const bibliographyPage = ({podcast}) => <div>TEST BIBLIOGRAPHY</div>;
+const aboutMePage = ({podcast}) => <div>TEST ABOUTME</div>;
+const theHistoryOfRomePage = ({podcast}) => <div>TEST THoR</div>;
+const audibleRecsPage = ({podcast}) => <div>TEST AUDIBLE RECS</div>;
+const contactPage = ({podcast}) => <div>TEST CONTACT</div>;
 
 module.exports = {
   supportPage,
