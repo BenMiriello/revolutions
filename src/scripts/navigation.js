2d6a19044c2d2cfdbfb7da76ca5a3b67646d1b2c
BenMiriello <benmiriello@gmail.com>
2021-08-02 23:18:36 -0400

    Allow navigating w/ back/forward buttons using interval

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,16 +1,35 @@
 const navPage = require('./navPage');
 
+const changePage = (element, data) => {
+  if (element.dataset.navTo) history.pushState({}, element.dataset.navTo, element.href);
+  newPage = navPage(data);
+  document.querySelector('#nav-page').innerHTML = '';
+  document.querySelector('#nav-page').append(newPage);
+  document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+}
+
 const navigation = (parent, data) => {
+  let clicked = false;
+  let whereami = location.href;
+  let reloadInterval = setInterval(() => {
+    if (location.href !== whereami) {
+      whereami = location.href;
+      if (!clicked) changePage(parent, data);
+      clicked = false;
+    }
+  }, 300);
+
+  document.addEventListener('onbeforeunload', () => {
+    clearInterval(reloadInterval);
+  });
+
   Array.from(parent.getElementsByTagName('a')).forEach(element => {
     if (element.href) {
       element.dataset.navTo = element.getAttribute('href');
       element.addEventListener('click', e => {
         e.preventDefault();
-        history.pushState({}, element.dataset.navTo, element.href);
-        newPage = navPage(data);
-        document.querySelector('#nav-page').innerHTML = '';
-        document.querySelector('#nav-page').append(newPage);
-        document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
+        changePage(element, data);
+        clicked = true;
       });
     }
   })
