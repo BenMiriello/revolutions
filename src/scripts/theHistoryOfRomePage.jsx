dbb7a64225c763e6a7c76b09d980b64c4c2e5d1c
BenMiriello <benmiriello@gmail.com>
2021-08-10 20:37:31 -0400

    Change THoR page to show links and not auto-redirect

--- src/scripts/theHistoryOfRomePage.jsx
+++ src/scripts/theHistoryOfRomePage.jsx
@@ -2,8 +2,16 @@ const createElement = require('./createElement');
 
 const theHistoryOfRomePage = podcast => {
   const thorPath = 'https://thehistoryofrome.typepad.com/';
-  location.replace(thorPath);
-  return(<div>Redirecting to <a href={thorPath}>The History of Rome</a></div>)
+  const iTunesPath = 'https://itunes.apple.com/us/podcast/the-history-of-rome/id261654474'
+
+  return(
+    <div style='line-height: 200%;'>
+      <h3>The History of Rome</h3>
+      <p style='margin-top: 1rem;'>I used to write The History of Rome. But you probably alredy knew that...</p>
+      <p>Website: <a href={thorPath}>{thorPath}</a></p>
+      <p>iTunes: <a href={iTunesPath}>{iTunesPath}</a></p>
+    </div>
+  )
 };
 
 module.exports = theHistoryOfRomePage;
